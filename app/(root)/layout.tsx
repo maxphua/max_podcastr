3f8b073d88977ec1e48ab89194a932ae32c27ece
Max Phua <maxphua90@gmail.com>
2024-06-04 16:30:37 +0800

    implement initial file and folder

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black-3">
+            <LeftSidebar />
+            {children}
+            <p className="text-white-1">RIGHT SIDEBAR</p>
+        </main>
+
+    </div>
+  );
+}
