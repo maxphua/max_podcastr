1970c9d8ce5a9f9f122e79abdd8421937ad76714
Max Phua <maxphua90@gmail.com>
2024-06-07 15:05:09 +0800

    Convex setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://pumped-tuna-0.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
