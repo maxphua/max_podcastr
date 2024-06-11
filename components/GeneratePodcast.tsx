1849f00a437239ccf9720cffcad9d2d8b2e0df5e
Max Phua <maxphua90@gmail.com>
2024-06-11 20:21:02 +0800

    Implement create podcast page

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,12 @@
+import { GeneratePodcastProps } from '@/types'
+import React from 'react'
+
+const GeneratePodcast = ({
+  setAudio, voiceType, voicePrompt, setAudioStorageId
+}: GeneratePodcastProps) => {
+  return (
+    <div>GeneratePodcast</div>
+  )
+}
+
+export default GeneratePodcast
