f19901cbffd3f3f68d5de09ab412b3725617b202
Max Phua <maxphua90@gmail.com>
2024-06-17 21:31:11 +0800

    Implement podcast detail page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import PodcastCard from '@/components/PodcastCard'
-// import { podcastData } from '@/constants'
+import { podcastData } from '@/constants'
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
 const Home = () => {
   // const tasks = useQuery(api.tasks.get);
-    const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
+    // const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
 
   return (
     <div className='mt-9 flex flex-col gap-9'>
@@ -16,7 +16,7 @@ const Home = () => {
           Trending Podcasts
         </h1>
 
-         <div className="podcast_grid">
+         {/* <div className="podcast_grid">
           {trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
             <PodcastCard 
               key={_id}
@@ -26,9 +26,9 @@ const Home = () => {
               podcastId={_id}
             />
           ))}
-        </div>
+        </div> */}
 
-        {/* <div className='podcast_grid'>
+        <div className='podcast_grid'>
         {podcastData.map(({id, title, description, imgURL}) => (
           <PodcastCard 
             key={id}
@@ -38,7 +38,7 @@ const Home = () => {
             podcastId={id}
           />
           ))}
-        </div> */}
+        </div>
 
       </section>
     </div>
--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,12 +1,25 @@
+import { PodcastCardProps } from '@/types'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const PodcastCard = ({
   imgUrl, title, description, podcastId
-}: {imgUrl: string, title: string, description: string, podcastId: number}) => {
+}: PodcastCardProps) => {
+
+  const router = useRouter()
+
+  const handleViews = () => {
+    // increase views
+
+    router.push(`/podcasts/${podcastId}`, {
+      scroll: true
+    })
+  }
+
   return (
-    <div className='cursor-point'>
-      <figure className='felx flex-col gap-2'>
+    <div className='cursor-pointer' onClick={handleViews}>
+      <figure className='flex flex-col gap-2'>
         <Image 
           src={imgUrl}
           width={174}
